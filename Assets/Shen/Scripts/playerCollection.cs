e22770a9e877060629108fa5dc499a237c0b5b97
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-10-11 23:32:30 +0200

    added basic item collection

---
+++ Assets/Shen/Scripts/playerCollection.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerCollection : MonoBehaviour
+{
+    public Transform collectionPoint;
+    public float collectionRange = 1f;
+    public LayerMask collectItemLayer;
+    public bool itemInRange;
+
+    public Text collectedItemText;
+    int numItemsCollected = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        itemInRange = Physics2D.OverlapCircle(collectionPoint.position, collectionRange, collectItemLayer);
+
+        if (itemInRange && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown("joystick button 0")))
+        {
+            Collider2D[] items = Physics2D.OverlapCircleAll(collectionPoint.position, collectionRange, collectItemLayer);
+
+            foreach (Collider2D collectibleItem in items)
+            {
+                numItemsCollected++;
+                collectedItemText.text = "Emeralds collected: " + numItemsCollected.ToString();
+                Destroy(collectibleItem.gameObject);
+            }
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (collectionPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(collectionPoint.position, collectionRange);
+    }
+
+}
