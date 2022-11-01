b9bc82aa60d555ba1cc9248c199627494d5364ab
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-11-01 18:15:32 +0200

    Added pick pocket mechanic

--- Assets/Shen/Scripts/playerCollection.cs
+++ Assets/Shen/Scripts/playerCollection.cs
@@ -11,11 +11,16 @@ public class playerCollection : MonoBehaviour
     public bool itemInRange;
 
     public Text collectedItemText;
+    public Text pickPocketedText;
     int numItemsCollected = 0;
+    int numItemsPickPocketed = 0;
 
     public GameObject[] emeralds;
 
     public GameObject pickUpText;
+    public GameObject pickPocketText;
+
+    public LayerMask[] collectiblesLayers;
 
     // Start is called before the first frame update
     void Start()
@@ -34,20 +39,46 @@ public class playerCollection : MonoBehaviour
 
             foreach (Collider2D collectibleItem in items)
             {
-                numItemsCollected++;
-                collectedItemText.text = "Emeralds collected: " + numItemsCollected.ToString() + "/3";
-               // emeralds[emeralds.Length] = collectibleItem.gameObject;
-                collectibleItem.gameObject.SetActive(false);
+                if(collectibleItem.gameObject.tag == "clue")
+                {
+                    numItemsCollected++;
+                    collectedItemText.text = "Emeralds collected: " + numItemsCollected.ToString() + "/3";
+                    // emeralds[emeralds.Length] = collectibleItem.gameObject;
+                    collectibleItem.gameObject.SetActive(false);
+                }
+                else if(collectibleItem.gameObject.tag == "pickPocketTag")
+                {
+                    numItemsPickPocketed++;
+                    pickPocketedText.text = "Items Pickpocketed: " + numItemsPickPocketed.ToString();
+                    collectibleItem.gameObject.tag = "Untagged";
+                    //change tag of enemy to cant be pickpocketed
+                    //Say this guard cant be pickpocketed anymore
+
+                }
+                
             }
         }
 
         if (itemInRange)
         {
-            pickUpText.SetActive(true);
+            Collider2D[] itemTags = Physics2D.OverlapCircleAll(collectionPoint.position, collectionRange, collectItemLayer);
+            foreach(Collider2D tag in itemTags)
+            {
+                if(tag.gameObject.tag == "clue")
+                {
+                    pickUpText.SetActive(true);
+                }else if(tag.gameObject.tag == "pickPocketTag")
+                {
+                    pickPocketText.SetActive(true);
+                }
+            }
+
+            
         }
         else
         {
             pickUpText.SetActive(false);
+            pickPocketText.SetActive(false);
         }
 
     }
