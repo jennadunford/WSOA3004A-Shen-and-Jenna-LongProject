7a6f4a20c40e4692877d688d48316edeb4cfe987
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-11-15 21:23:50 +0200

    Updated Whistle test

---
+++ Assets/Integrations/Scripts/Player_Whistle.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Whistle : MonoBehaviour
+{
+    public Transform whistlePoint;
+    public float whistleRange = 5f;
+    public LayerMask enemyLayer;
+    public bool whistling = false;
+
+    
+    public float totalWhistleTime = 1f;
+    public float remainingWhistleTime = 0f;
+    //public bool dashing = false;
+    public float whistleCoolDownTime = 0.5f;
+    public bool whistleCoolDownOver = true;
+
+    public GameObject playerObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainingWhistleTime = totalWhistleTime;
+        whistling = false;
+        whistleCoolDownOver = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!whistling && (Input.GetKeyDown(KeyCode.K) /*|| Input.GetKeyDown("joystick button 1")*/))
+        {
+            if (whistleCoolDownOver && remainingWhistleTime == totalWhistleTime)
+            {
+                whistling = true;
+                playerObject.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+        }
+
+
+        if (remainingWhistleTime <= 0)
+        {
+            //playerRigidBody.velocity = new Vector2(0f, 0f);
+            whistling = false;
+            remainingWhistleTime = totalWhistleTime;
+            //dashCoolDownTime -= Time.deltaTime;
+            whistleCoolDownOver = false;
+            playerObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+
+        if (whistling)
+        {
+            //need to make this an if condition based on enemy tag as well
+            Collider2D[] enemies = Physics2D.OverlapCircleAll(whistlePoint.position, whistleRange, enemyLayer);
+
+            foreach (Collider2D distractedEnemy in enemies)
+            {
+                Debug.Log(distractedEnemy.gameObject.name + " is distracted now");
+            }
+
+            remainingWhistleTime -= Time.deltaTime; //fixed delta time is for fixed update, update just uses delta time
+            
+        }
+
+        if (!whistleCoolDownOver)
+        {
+            whistleCoolDownTime -= Time.deltaTime;
+        }
+
+        if (whistleCoolDownTime <= 0)
+        {
+            whistleCoolDownOver = true;
+            whistleCoolDownTime = 0.5f;
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (whistlePoint == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(whistlePoint.position, whistleRange);
+    }
+
+}
